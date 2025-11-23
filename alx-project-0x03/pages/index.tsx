a5864749daccaf77739a8bf8d6923ecce9359e98
pius wanyangu <ndubipius96@gmail.com>
2025-11-23 22:27:29 +0300

    adding styles

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,23 +1,9 @@
-// pages/index.tsx
-
-import React from 'react';
-import Header from '@/components/layouts/Header'; // Ensure this path is correct
-
-const IndexPage: React.FC = () => {
+const Home = () => {
   return (
-    <div>
-      {/* 1. Add the Header Component here */}
-      <Header /> 
-      
-      {/* 2. Your main page content follows */}
-      <main style={{ padding: '20px' }}>
-        <h1>This is the Main Index Page (/)</h1>
-        <p>You can use the navigation links above to go to /home and /about.</p>
-        
-        {/* You can also put the content that was originally in index.tsx here */}
-      </main>
-    </div>
-  );
-};
+   <div>
+    <h1> Welcome to Splash App </h1>
+   </div>
+  )
+}
 
-export default IndexPage;
+export default Home;
